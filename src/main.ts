7c23af7d5e2aa0fd0ebb73a1c093954f40d6e783
NNNNzs <nnnnzs@vip.qq.com>
2022-04-05 20:08:57 +0800

    change webHashHistory

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { createApp, defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import RootVue from './layout/Root.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import STView from './view/ST.vue'
 
 const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHistory('/upload'),
+  history: createWebHashHistory('/upload'),
   routes: [
     {
       name:'index',
